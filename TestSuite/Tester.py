151795ef850191848500979b7438ae055973f7f7
Tomer <tomash22123@gmail.com>
2018-05-26 20:33:33 +0300

    Added test package

---
+++ TestSuite/Tester.py
@@ -0,0 +1,12 @@
+import unittest
+from Controller.Control import Controller
+
+
+class Tester(unittest.TestCase):
+
+    def test_negative(self):
+        self.assertFalse(...)
+
+
+if __name__ == '__main__':
+    C = Controller()
